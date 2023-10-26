2a702ff707ceefb80b1fcc6cfa03abb4572cc9f9
CherrySUKURA <1938083884@qq.com>
2023-10-26 22:03:42 +0800

    first commit

---
+++ package_one/功能演示SQL.sql
@@ -0,0 +1,39 @@
+-- select * from users
+
+-- select username,password from users;
+
+-- insert into users (username,password) values ('ch','ch')
+
+-- update users set password = 888888 where id = 5;
+
+-- update users set username = 'tony stark', password = 123 where id = 5;
+
+-- delete from users where id = 5;
+
+-- select * from users where status = 0
+
+-- select * from users where id > 2
+
+-- select * from users where id != 3
+
+-- select * from users where id between 1 and 4
+
+-- select * from users where id = 1 and status = 0
+
+-- select * from users where id = 1 or status = 1
+
+-- select * from users order by status;
+
+-- select * from users order by status asc;
+
+-- select * from users order by status desc;
+
+-- select * from users order by status desc , username asc;
+
+-- select count(*) from users where status = 0;
+
+-- select count(*) as total from users where status = 0;
+
+-- select username as uname , password as pwd from users;
+
+-- select * from users
